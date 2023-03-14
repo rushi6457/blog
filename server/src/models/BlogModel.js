37c28f4e62571447b59c696e7ad30e8bf670aeb4
rushi6457 <rushiypatil@gmail.com>
2023-03-14 19:35:44 +0530

    blogmodel

--- server/src/models/BlogModel.js
+++ server/src/models/BlogModel.js
@@ -1,5 +1,30 @@
 const mongoose  = require('mongoose')
 
 const BlogSchema = new mongoose.Schema({
-    
+    title:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    blog:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    image:{
+        type:String
+    },
+    author:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'user'
+    },
+    comments:{
+        type:String
+    },
+    likes:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'user'
+    }
+},{
+    timestamps:true
 })
