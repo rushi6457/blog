7c2b46ca0b8a10d33ffd1a237829d1294b0ad86f
rushi6457 <rushiypatil@gmail.com>
2023-03-16 17:24:13 +0530

    login route

--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { Signup } = require('../controllers/userController');
+const { Signup, Login } = require('../controllers/userController');
 const router = express.Router();
 
 router.post("/signup",Signup)
-
+router.post("/login",Login)
 
 module.exports = router
